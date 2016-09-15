2cfb05cdae9bb6daac9c17c410ffa1038b4d7153
Jediah <jediahkatz@gmail.com>
2016-09-14 20:44:44 -0400

    Add Book model, Book admin class, and change store template

--- bookstore/urls.py
+++ bookstore/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 
+admin.site.site_header = 'Bookstore administration'
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^store/', include('store.urls'), name='store'),
